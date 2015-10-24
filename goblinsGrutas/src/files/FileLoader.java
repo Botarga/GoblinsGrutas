515abbc5ab25ca664c0686d43e4a83cca83f0b7d
botarga <botarga@botarga-System-Product-Name>
2015-10-25 00:54:35 +0200

    .

--- goblinsGrutas/src/files/FileLoader.java
+++ goblinsGrutas/src/files/FileLoader.java
@@ -2,6 +2,8 @@ package files;
 
 import java.util.*;
 import goblinsgrutas.Personaje;
+import java.io.*;
+
 /**
  *
  * @author botarga
@@ -10,6 +12,11 @@ public class FileLoader {
     public static Personaje leerPersonaje(String path){
         Personaje p = null;
         
+        try(BufferedReader bf = new BufferedReader(new FileReader(path))){
+            
+        }catch(IOException e){
+            System.err.println("Error: " + e.getMessage());
+        }
         
         return p;
     }
