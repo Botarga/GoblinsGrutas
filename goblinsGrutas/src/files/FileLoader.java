30a6a057f437e4f057fdd16df5c169e3645d835c
botarga <botarga@botarga-System-Product-Name>
2015-10-25 00:52:18 +0200

    añadida FileLoader

---
+++ goblinsGrutas/src/files/FileLoader.java
@@ -0,0 +1,16 @@
+package files;
+
+import java.util.*;
+import goblinsgrutas.Personaje;
+/**
+ *
+ * @author botarga
+ */
+public class FileLoader {
+    public static Personaje leerPersonaje(String path){
+        Personaje p = null;
+        
+        
+        return p;
+    }
+}
