e15e9ab73a17d25486ece7eb3f00a4fa16941065
botarga <botarga@botarga-System-Product-Name>
2015-10-25 12:59:17 +0100

    Creado script ejecutable y algunos menus

--- goblinsGrutas/src/goblinsgrutas/Main.java
+++ goblinsGrutas/src/goblinsgrutas/Main.java
@@ -1,10 +1,10 @@
 package goblinsgrutas;
 
-import files.FileLoader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.*;
-import javax.xml.bind.JAXBException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  *
@@ -12,11 +12,14 @@ import javax.xml.bind.JAXBException;
  */
 public class Main {
     private static boolean windows;
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private List<Personaje> personajes;
     
     public static String[] habilidades = {
         "Combate", "Ataque", "Defensa", "Conocimientos", "Latrocinio", "Magia"
         , "Sociales"
     };
+    
     
     public static void clearConsole(){
         if(windows)
@@ -24,14 +27,101 @@ public class Main {
                 System.out.println();
         else
             System.out.printf("\033[2J\033[H");
-            
-        
     }
+    
+    
+    public static void gestionPersonajesMenu(){
+        int opcion;
+        boolean salir = false;
+        
+        do{
+            clearConsole();
+        
+            System.out.println("Menu de gestión de personajes");
+            System.out.println("1 Cargar personaje de fichero");
+            System.out.println("2 Crear personaje desde consola");
+            System.out.println("3 Guardar personajes en un directorio");
+            System.out.println("0 Volver");
+            
+            opcion = readInt("Introduce una opcion");
+            
+            switch(opcion){
+                case 0:
+                    salir = true;
+                    break;
+                    
+                case 1:
+                    break;
+                    
+                case 2:
+                    break;
+                    
+                case 3:
+                    break;
+                    
+                default:
+                    break;               
+                      
+            }
+        }while(!salir);
+    }
+    
+    
+    public static String readString(String question){
+        String data = null;
+        
+        System.out.print(question);
+        try {
+            data = br.readLine();
+        }catch(IOException e){
+            System.err.println("Error: " + e.getMessage());
+        }
+        
+        return data;
+    }
+    
+    public static int readInt(String question){
+        return Integer.parseInt(readString(question));
+    }
+    
+    
+    public static void mainMenu(){
+        int opcion;
+        boolean salir = false;
+        
+        do{
+            clearConsole();
+            
+            System.out.println("Bienvenido al asistente de Goblins and rutas");
+            System.out.println("Que quieres hacer?\n");
+            System.out.println("1 Gestión de personajes");
+            System.out.println("0 Salir");
+            
+            opcion = readInt("Introduce una opcion: ");
+            
+            switch(opcion){
+                case 0:
+                    salir = true;
+                    break;
+                    
+                case 1:
+                    gestionPersonajesMenu();
+                    break;
+                                      
+                default:
+                    break;
+            }
+        }while(!salir);
+    }
+    
     
     public static void main (String[] args) throws Exception{
         windows = System.getProperty("os.name").startsWith("Win");
         clearConsole();
-        Personaje p = FileLoader.leerPersonaje("build/personajes/personaje1.txt");
-        p.mostrarInfo();
+        
+        mainMenu();
+        
+        System.out.println("Hasta la proxima!");
     }
+    
 }
